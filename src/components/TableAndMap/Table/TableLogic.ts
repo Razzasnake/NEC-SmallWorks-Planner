99134b08441246a88e433aaa5408ede70029ca93
Tim <tjgambs@gmail.com>
2020-05-13 18:37:38 -0500

    Predict column types, closes #2

--- src/components/TableAndMap/Table/TableLogic.ts
+++ src/components/TableAndMap/Table/TableLogic.ts
@@ -1,5 +1,5 @@
-import { ColDef, ColGroupDef } from 'ag-grid-community'
-import UploadedFile from '@/entities/UploadedFile'
+import { ColDef, ColGroupDef } from "ag-grid-community"
+import UploadedFile from "@/entities/UploadedFile"
 
 export const defaultColDef: ColDef = {
   sortable: true,
@@ -11,17 +11,39 @@ export default class TableLogic {
 
   public columnDefs: (ColDef | ColGroupDef)[] = []
 
+  private computeColumnTypes(uploadedFile: UploadedFile) {
+    const columnTypes: { number: number, string: number }[] = []
+    for (let i = 0; i < uploadedFile.data[0].data.length; i++) {
+      columnTypes.push({ number: 0, string: 0 })
+    }
+    uploadedFile.data.slice(uploadedFile.firstRowHeader ? 1 : 0).forEach(row => {
+      row.data.forEach((col, colIndex) => {
+        if (isNaN(col)) {
+          columnTypes[colIndex].string += 1
+        } else {
+          columnTypes[colIndex].number += 1
+        }
+      })
+    })
+    return columnTypes.map(meta => {
+      return (meta.number / (meta.number + meta.string)) > 0.95 ? "number" : "text"
+    })
+  }
+
   constructor(uploadedFile: UploadedFile) {
+    const columnTypes = this.computeColumnTypes(uploadedFile)
     const generatedCols: (ColDef | ColGroupDef)[] = uploadedFile.data[0].data.map((_, index) => {
       if (uploadedFile.firstRowHeader) {
         return {
           headerName: uploadedFile.data[0].data[index],
-          field: index.toString()
+          field: index.toString(),
+          filter: columnTypes[index]
         }
       } else {
         return {
           headerName: `Column ${(index + 1).toString()}`,
-          field: index.toString()
+          field: index.toString(),
+          filter: columnTypes[index]
         }
       }
     })
