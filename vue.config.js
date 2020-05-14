edf38c0b39146d237d23414720d5a27f3f7d3a7e
Tim <tjgambs@gmail.com>
2020-05-14 18:59:53 -0500

    Delete vue.config.js, keep prefetch

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const path = require('path')
-
-module.exports = {
-  chainWebpack: (config) => {
-    config.plugins.delete('prefetch')
-  }
-}
