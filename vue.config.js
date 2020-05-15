716060cc13a4d08baa89bd46a9345938c74c80f2
Tim <tjgambs@gmail.com>
2020-05-14 19:17:04 -0500

    Added a sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+
+const paths = [
+  '/examples'
+]
+
 module.exports = {
   chainWebpack: (config) => {
     config.plugins.delete('prefetch')
+  },
+  configureWebpack: {
+    plugins: [
+      // new BundleAnalyzerPlugin(),
+      new SitemapPlugin(process.env.VUE_APP_BASE_URL, paths)
+    ]
   }
 }
