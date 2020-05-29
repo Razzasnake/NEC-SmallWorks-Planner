bd309d48c96b6a87e1fa98f79fbd33d314af34f6
Tim <tjgambs@gmail.com>
2020-05-28 23:23:57 -0500

    dont extend with center of map

--- src/components/TableAndMap/GoogleMap/GoogleMap.vue
+++ src/components/TableAndMap/GoogleMap/GoogleMap.vue
@@ -251,6 +251,7 @@ export default class GoogleMap extends Vue {
         return;
       }
       this.map = new google.maps.Map(mapEl, {
+        center: new google.maps.LatLng(39.8283, -98.5795),
         zoom: 5,
         disableDefaultUI: true,
         clickableIcons: false,
@@ -272,7 +273,6 @@ export default class GoogleMap extends Vue {
 
   private updateBounds() {
     const bounds = new google.maps.LatLngBounds();
-    bounds.extend(new google.maps.LatLng(39.8283, -98.5795))
     this.markers
       .filter((_, index) => !this.hiddenMarkerIndices.has(index))
       .forEach(marker => {
