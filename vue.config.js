743c9f3f1b8ace58739062f81119ea747059e23e
Tim <tjgambs@gmail.com>
2020-05-30 00:46:53 -0500

    Added back no prefetch

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,9 @@ const paths = [
 ]
 
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+  },
   configureWebpack: {
     plugins: [
       // new BundleAnalyzerPlugin(),
