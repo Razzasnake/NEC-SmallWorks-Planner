a6267b129cafdb0ee6dec214962b23567e32df1f
Tim <tjgambs@gmail.com>
2020-07-16 22:19:40 -0500

    Update are you sure modal

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <NavBar @jumpTo="jumpTo"></NavBar>
       <v-main>
         <router-view></router-view>
+        <v-dialog v-model="areYouSureModal" max-width="400" @outside="cancelLeave">
+          <v-card>
+            <v-card-title class="headline">Are you sure you want to leave?</v-card-title>
+            <v-card-text>You will lose all uploaded markers, shapefiles, filters and sortings.</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="cancelLeave">Cancel</v-btn>
+              <v-btn color="primary" text @click="confirmLeave">Confirm</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-main>
     </v-app>
   </div>
@@ -23,21 +34,31 @@ import { reset } from "@/store/exploreStore";
   }
 })
 export default class App extends Vue {
+  private areYouSureModal = false;
+  private pathToLeaveTo: { name: string } | null = null;
+
   private jumpTo(location: { name: string }) {
     if (location.name !== this.$route.name) {
       if (this.$route.name === "Explore") {
-        this.$buefy.dialog.confirm({
-          message: "Are you sure you want to leave?",
-          confirmText: "Yes",
-          onConfirm: () => {
-            this.$router.push(location);
-            reset();
-          }
-        });
+        this.areYouSureModal = true;
+        this.pathToLeaveTo = location;
       } else {
         this.$router.push(location);
       }
     }
+  }
+
+  private confirmLeave() {
+    if (this.pathToLeaveTo) {
+      this.$router.push(this.pathToLeaveTo);
+    }
+    this.cancelLeave();
+    reset();
+  }
+
+  private cancelLeave() {
+    this.areYouSureModal = false;
+    this.pathToLeaveTo = null;
   }
 }
 </script>
