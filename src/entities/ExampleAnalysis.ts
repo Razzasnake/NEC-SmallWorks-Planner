48a165045d93823595f599d089fad84d6bb71674
Tim <tjgambs@gmail.com>
2020-07-27 22:09:28 -0500

    Delete unused getter

--- src/entities/ExampleAnalysis.ts
+++ src/entities/ExampleAnalysis.ts
@@ -25,33 +25,6 @@ export default class ExampleAnalysis implements ExampleAnalysisI {
     firstRowHeader: boolean
   };
 
-  public get formattedCreatedOn(): string {
-    const year = this.createdOn.getFullYear().toString();
-    const month = this.createdOn
-      .getMonth()
-      .toString()
-      .padStart(2, "0");
-    const day = this.createdOn
-      .getDate()
-      .toString()
-      .padStart(2, "0");
-    let hours = this.createdOn.getHours();
-    let amPm = "AM";
-    if (hours > 12) {
-      hours = hours - 12;
-      amPm = "PM";
-    } else if (hours === 0) {
-      hours = 12
-    }
-    const minutes = this.createdOn
-      .getMinutes()
-      .toString()
-      .padStart(2, "0");
-    return `${month}-${day}-${year} ${hours
-      .toString()
-      .padStart(2, "0")}:${minutes} ${amPm}`;
-  }
-
   constructor(obj: ExampleAnalysisI) {
     Object.assign(this, obj)
   }
