a0093384ff125360816d95e38e9e7b772523c9b1
Tim Gamble <tjgambs@gmail.com>
2020-07-29 12:29:49 -0500

    Add features to sitemap

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,15 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const paths = [
   '/',
   '/examples',
-  '/features'
+  '/features',
+  '/feature/map-your-csv-or-excel-locational-data',
+  '/feature/filter-with-shapes',
+  '/feature/unlimited-geocoding',
+  '/feature/geojson-and-shapefile-layers',
+  '/feature/supports-50-000-markers',
+  '/feature/heat-map-layer',
+  '/feature/automation',
+  '/feature/helpful-table-footers'
 ]
 
 module.exports = {
