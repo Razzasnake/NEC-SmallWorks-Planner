cf0d01e9b14d7440457ca3a32d7163c6e9b6a653
Tim Gamble <tjgambs@gmail.com>
2020-07-31 00:34:08 -0500

    Use base page component for features section

--- src/components/Features/Feature/Feature.vue
+++ src/components/Features/Feature/Feature.vue
@@ -1,39 +1,26 @@
 <template>
-  <div v-editable="blok" class="full-height">
-    <v-img
-      class="image-header"
-      :src="blok.preview.filename"
-      gradient="to top, rgba(16, 84, 106, .75), rgba(16, 84, 106, .75)"
-    >
-      <div class="section">
-        <div class="section-header">
-          <div>
-            <div class="text-h4" v-html="createHtml(blok.title)"></div>
-            <div class="text-subtitle-1" v-html="createHtml(blok.subtitle)"></div>
-          </div>
-        </div>
-      </div>
-    </v-img>
-    <div class="section">
-      <div class="feature">
-        <div v-html="createHtml(blok.content)"></div>
-        <iframe
-          v-if="blok.youtubeUrl.url"
-          width="100%"
-          height="500"
-          :src="blok.youtubeUrl.url"
-          frameborder="0"
-        ></iframe>
-      </div>
-    </div>
-    <Footer />
-  </div>
+  <BasePage
+    :title="blok.title"
+    :subtitle="blok.subtitle"
+    :img="blok.preview.filename"
+    v-editable="blok"
+  >
+    <div v-html="createHtml(blok.content)"></div>
+    <iframe
+      v-if="blok.youtubeUrl.url"
+      width="100%"
+      height="500"
+      :src="blok.youtubeUrl.url"
+      frameborder="0"
+    ></iframe>
+  </BasePage>
 </template>
 <script lang='ts'>
 import { Component, Vue, Prop } from "vue-property-decorator";
 import StoryblokClient from "storyblok-js-client";
 import Footer from "@/components/Home/Footer/Footer.vue";
 import FeatureI from "@/entities/storyblok/Feature";
+import BasePage from "@/components/Shared/Page/Page.vue";
 
 const Storyblok = new StoryblokClient({
   accessToken: process.env.VUE_APP_STORYBLOK_TOKEN,
@@ -44,6 +31,7 @@ const Storyblok = new StoryblokClient({
  */
 @Component({
   components: {
+    BasePage,
     Footer,
   },
 })
@@ -59,20 +47,3 @@ export default class Feature extends Vue {
   }
 }
 </script>
-<style lang='scss' scoped>
-.image-header {
-  display: flex;
-  align-items: center;
-  height: calc(200px + 6rem);
-  width: 100%;
-  .section-header {
-    max-width: 1215px;
-    margin: auto;
-    color: #eeeeee;
-  }
-}
-.feature {
-  max-width: 1215px;
-  margin: auto;
-}
-</style>
--- src/components/Shared/Page/Page.vue
+++ src/components/Shared/Page/Page.vue
@@ -8,8 +8,10 @@
       <div class="section">
         <div class="section-header">
           <div>
-            <div class="text-h4">{{ title }}</div>
-            <div class="text-subtitle-1">{{ subtitle }}</div>
+            <div class="text-h4" v-if="titleIsString">{{ title }}</div>
+            <div class="text-h4" v-else v-html="createHtml(title)"></div>
+            <div class="text-subtitle-1" v-if="subtitleIsString">{{ subtitle }}</div>
+            <div class="text-subtitle-1" v-else v-html="createHtml(subtitle)"></div>
           </div>
         </div>
       </div>
@@ -25,7 +27,12 @@
 </template>
 <script lang='ts'>
 import { Component, Prop, Vue } from "vue-property-decorator";
+import StoryblokClient from "storyblok-js-client";
 import Footer from "@/components/Home/Footer/Footer.vue";
+
+const Storyblok = new StoryblokClient({
+  accessToken: process.env.VUE_APP_STORYBLOK_TOKEN,
+});
 
 /**
  * Generic Page component
@@ -40,24 +47,35 @@ export default class Page extends Vue {
    * Title
    */
   @Prop()
-  private title!: string;
+  private title!: string | object;
   /**
    * Subtitle
    */
   @Prop()
-  private subtitle!: string;
+  private subtitle!: string | object;
   /**
    * Image to appear behind the header
    */
   @Prop({ default: null })
   private img!: string | null;
+
+  private get titleIsString() {
+    return typeof this.title === "string";
+  }
+
+  private get subtitleIsString() {
+    return typeof this.subtitle === "string";
+  }
+
+  private createHtml(richText: object) {
+    return Storyblok.richTextResolver.render(richText);
+  }
 }
 </script>
 <style lang='scss' scoped>
 .image-header {
   display: flex;
   align-items: center;
-  text-align: center;
   height: calc(200px + 6rem);
   width: 100%;
   .section-header {
