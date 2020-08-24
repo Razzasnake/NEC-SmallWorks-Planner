f1a9be2cec5b17d9f56ac4e9e67acf69d0899f9e
Tim <tjgambs@gmail.com>
2020-08-23 22:27:08 -0500

    latitude -> latitudes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,7 +30,7 @@ export default class Home extends Vue {
     const description = document.getElementsByName("description");
     if (description.length) {
       (description[0] as HTMLMetaElement).content =
-        "Visualize your location data in an interactive map. Upload an excel or csv file with addresses or latitude and longitudes to get started.";
+        "Visualize your location data in an interactive map. Upload an excel or csv file with addresses or latitudes and longitudes to get started.";
     }
   }
 }
