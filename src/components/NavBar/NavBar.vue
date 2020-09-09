bb96189dc99dbe701c47df460f2b171f86997e9a
Tim Gamble <tjgambs@gmail.com>
2020-09-08 23:51:36 -0500

    Better solution

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -80,8 +80,11 @@ export default class NavBar extends Vue {
 
   private get fileName() {
     if (state.uploadedFile) {
-      const fileNameArr = state.uploadedFile.fileName.split(".");
-      return fileNameArr.slice(0, fileNameArr.length - 2).join(".") + ".csv";
+      if (state.uploadedFile.fileName.indexOf(".") > -1) {
+        const fileNameArr = state.uploadedFile.fileName.split(".");
+        return fileNameArr.slice(0, fileNameArr.length - 2).join(".") + ".csv";
+      }
+      return state.uploadedFile.fileName;
     }
     return "";
   }
