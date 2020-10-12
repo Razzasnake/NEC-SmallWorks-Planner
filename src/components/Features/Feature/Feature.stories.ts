1c071a9dbc01c3d858c3722789d1b0afb6c400a7
Tim <tjgambs@gmail.com>
2020-10-12 12:17:05 -0500

    setup the stories

--- src/components/Features/Feature/Feature.stories.ts
+++ src/components/Features/Feature/Feature.stories.ts
@@ -7,21 +7,42 @@ export default {
   title: 'Features/Feature'
 }
 
-const _Feature = () => ({
+const FeatureTemplate = (args, { argTypes }) => ({
   components: { Feature },
   template: `
     <Feature
       :feature="feature"
       @finish="finish"
     ></Feature>`,
-  props: {
-    feature: {
-      default: features[0]
-    }
-  },
+  props: Object.keys(argTypes),
   methods: {
     finish: action('finish')
   }
 })
 
-export { _Feature }
+export const MapYourLocationData = FeatureTemplate.bind({})
+MapYourLocationData.args = { feature: features[0] }
+
+export const Filterable = FeatureTemplate.bind({})
+Filterable.args = { feature: features[1] }
+
+export const UnlimitedGeocoding = FeatureTemplate.bind({})
+UnlimitedGeocoding.args = { feature: features[2] }
+
+export const GeojsonAndShapefileLayers = FeatureTemplate.bind({})
+GeojsonAndShapefileLayers.args = { feature: features[3] }
+
+export const SupportsManyMarkers = FeatureTemplate.bind({})
+SupportsManyMarkers.args = { feature: features[4] }
+
+export const HeatMapLayer = FeatureTemplate.bind({})
+HeatMapLayer.args = { feature: features[5] }
+
+export const CategoricalGrouping = FeatureTemplate.bind({})
+CategoricalGrouping.args = { feature: features[6] }
+
+export const Automation = FeatureTemplate.bind({})
+Automation.args = { feature: features[7] }
+
+export const HelpfulTableFooters = FeatureTemplate.bind({})
+HelpfulTableFooters.args = { feature: features[8] }
