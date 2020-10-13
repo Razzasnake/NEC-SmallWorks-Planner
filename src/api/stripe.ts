fd23de7872fd723ea5623fb9f6790a1b49d30b6c
Tim <tjgambs@gmail.com>
2020-10-12 21:38:36 -0500

    guess at true url

--- src/api/stripe.ts
+++ src/api/stripe.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   getCustomerTier(email: string) {
-    const url = "https://tableandmap.com/.netlify/getCustomerTier";
+    const url = "https://tableandmap.com/.netlify/functions/getCustomerTier";
     return axios.post(url, { email }).then((resp) => {
       return parseInt(resp.data.data);
     });
