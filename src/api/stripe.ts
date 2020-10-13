a35b74e353bed2778ccd8b14837bbe36cdf7f7a1
Tim <tjgambs@gmail.com>
2020-10-13 16:50:32 -0500

    On fail, return no tier

--- src/api/stripe.ts
+++ src/api/stripe.ts
@@ -5,6 +5,8 @@ export default {
     const url = "https://tableandmap.com/.netlify/functions/getCustomerTier";
     return axios.post(url, { email }).then((resp) => {
       return parseInt(resp.data);
+    }).catch(() => {
+      return 0;
     });
   }
 }
