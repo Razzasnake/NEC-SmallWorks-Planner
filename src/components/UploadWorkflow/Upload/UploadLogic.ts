83f9c60deb15cfbbfe78a7785945979f5bd6550f
Tim <tjgambs@gmail.com>
2020-10-15 11:10:29 -0500

    Fix cta

--- src/components/UploadWorkflow/Upload/UploadLogic.ts
+++ src/components/UploadWorkflow/Upload/UploadLogic.ts
@@ -1,79 +1,68 @@
 export default class UploadLogic {
+  public static initDropZoneIds: string[] = [];
   public static initDropZone(id: string, accept: string, uploadCallback: (file: File) => void, multiple: boolean) {
-    let dropArea = document.getElementById(id);
+    if (UploadLogic.initDropZoneIds.indexOf(id) > -1) {
+      return;
+    }
+    const dropArea = document.getElementById(id);
     if (dropArea) {
-      if (dropArea.parentNode) {
-        dropArea.parentNode.replaceChild(dropArea.cloneNode(true), dropArea);
-        dropArea = document.getElementById(id);
-      }
+      UploadLogic.initDropZoneIds.push(id);
       ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
-        if (dropArea) {
-          dropArea.addEventListener(
-            eventName,
-            (e) => {
-              e.preventDefault();
-              e.stopPropagation();
-            },
-            false
-          );
-        }
-      });
-
-      ["dragenter", "dragover"].forEach((eventName) => {
-        if (dropArea) {
-          dropArea.addEventListener(
-            eventName,
-            (e) => {
-              if (dropArea) {
-                dropArea.classList.add("highlight");
-              }
-            },
-            false
-          );
-        }
-      });
-
-      ["dragleave", "drop"].forEach((eventName) => {
-        if (dropArea) {
-          dropArea.addEventListener(
-            eventName,
-            (e) => {
-              if (dropArea) {
-                dropArea.classList.remove("highlight");
-              }
-            },
-            false
-          );
-        }
-      });
-
-      if (dropArea) {
         dropArea.addEventListener(
-          "drop",
+          eventName,
           (e) => {
-            const dt = e.dataTransfer;
-            if (dt) {
-              const fnc = (file: File) => {
-                if (
-                  accept.split(",").filter((_) => file.name.endsWith(_))
-                    .length > 0
-                ) {
-                  /* If a supported file. */
-                  uploadCallback(file);
-                }
-              };
-              if (multiple) {
-                Array.from(dt.files).forEach(file => {
-                  fnc(file);
-                });
-              } else {
-                fnc(dt.files[0]);
-              }
-            }
+            e.preventDefault();
+            e.stopPropagation();
           },
           false
         );
-      }
+      });
+
+      ["dragenter", "dragover"].forEach((eventName) => {
+        dropArea.addEventListener(
+          eventName,
+          (e) => {
+            dropArea.classList.add("highlight");
+          },
+          false
+        );
+      });
+
+      ["dragleave", "drop"].forEach((eventName) => {
+        dropArea.addEventListener(
+          eventName,
+          (e) => {
+            dropArea.classList.remove("highlight");
+          },
+          false
+        );
+      });
+
+      dropArea.addEventListener(
+        "drop",
+        (e) => {
+          const dt = e.dataTransfer;
+          if (dt) {
+            const fnc = (file: File) => {
+              if (
+                accept.split(",").filter((_) => file.name.endsWith(_))
+                  .length > 0
+              ) {
+                /* If a supported file. */
+                uploadCallback(file);
+              }
+            };
+            if (multiple) {
+              Array.from(dt.files).forEach(file => {
+                fnc(file);
+              });
+            } else {
+              fnc(dt.files[0]);
+            }
+          }
+        },
+        false
+      );
     }
   }
 }
