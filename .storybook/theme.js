341ce8d381f89c3f232ebb5a1d1899983adb41d6
Tim <tjgambs@gmail.com>
2020-11-18 23:51:45 -0600

    Upgrade to storybook v6.1

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,7 +3,5 @@ import { create } from "@storybook/theming/create";
 export default create({
   base: "light",
   appContentBg: "#fafafa",
-  brandTitle: "Table & Map",
-  brandUrl: "https://tableandmap.com",
   fontBase: "Roboto, sans-serif"
 });
